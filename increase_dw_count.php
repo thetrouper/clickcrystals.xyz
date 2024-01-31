8df9a232e2ce54eaefae985200ec258938fd18bb
Tutla <91965613+ayaanibrahimtutla@users.noreply.github.com>
2024-01-31 19:37:40 +0530

    Update increase_dw_count.php

--- increase_dw_count.php
+++ increase_dw_count.php
@@ -1,15 +1,15 @@
 <?php
 $filename = 'dc.txt';
 
-// Read the current count from the file
+
 $count = (int)file_get_contents($filename);
 
-// Increment the count
+
 $count++;
 
-// Write the updated count back to the file
+
 file_put_contents($filename, $count);
 
-// Return the updated count as JSON
+
 echo json_encode(['count' => $count]);
 ?>
