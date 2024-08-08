660ad160dd501f6b2a89ccd32481190f6b9b1d99
Yuri <yuridev2024@gmail.com>
2024-08-08 17:45:05 +0530

    fix copyright Y issue

--- components/ui/Footer/FooterInfo.tsx
+++ components/ui/Footer/FooterInfo.tsx
@@ -5,12 +5,12 @@ export default function FooterInfo() {
   return (
     <>
       <div className="w-full border-t border-t-blue-400" />
-      <div className="pt-6 md:pt-10 md:mx-8 grid justify-items-center md:flex md:justify-between">
+      <div className="py-4 md:py-10 md:mx-8 grid justify-items-center md:flex md:justify-between items-center">
         <Info />
+        <span className="font-bold text-slate-100">
+          Copyright &copy; ItziSpyder & Contributors 2022-{new Date().getFullYear()}
+        </span>
         <Socials />
-      </div>
-      <div className="flex flex-row justify-center text-white text-sm pb-12 pt-4">
-        Copyright &copy; ImproperIssues & Contributors 2022-{new Date().getFullYear()}
       </div>
     </>
   )
