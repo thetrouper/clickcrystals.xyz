f4ec514262773c9a390c06089c43550d8e6a8dc4
Yuri <yuridev2024@gmail.com>
2024-08-08 17:58:10 +0530

    add links to icons (I forgot to)

--- components/ui/Footer/Socials.tsx
+++ components/ui/Footer/Socials.tsx
@@ -6,9 +6,9 @@ import { Social } from './Social';
 export default function Socials() {
   return (
     <ul className="flex mt-0">
-      <Social icon={faGithub} url="#" className='text-slate-800' />
-      <Social icon={faDiscord} url="#" className='text-blue-600' />
-      <Social icon={faYoutube} url="#" className='text-red-600' />
+      <Social icon={faGithub} url="https://github.com/clickcrystals-development/ClickCrystals" className='text-slate-800' />
+      <Social icon={faDiscord} url="https://discord.gg/zg3ge9VTgr" className='text-blue-600' />
+      <Social icon={faYoutube} url="https://www.youtube.com/@itzispyder" className='text-red-600' />
     </ul>
   )
 }
