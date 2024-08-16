b8a0a0591215c593b5c47f847fe1078abbef2ccf
Yuri <yuridev2024@gmail.com>
2024-08-16 22:35:55 +0530

    add scale effect to script cards

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -2,15 +2,17 @@ type ContainerProps = {
   children: React.ReactNode;
   tapScale?: number | string;
   className?: string;
+  hoverScale?: number | string;
 }
 
 import { motion } from "framer-motion";
 
-export const Container = ({children, tapScale = 0.95, className = "", ...props}: ContainerProps) => {
+export const Container = ({children, tapScale = 0.95, className = "", hoverScale = 1, ...props}: ContainerProps) => {
   return (
     <motion.div
       className={`transition scale-1 inline cursor-pointer ${className}`}
       whileTap={{ scale: tapScale }}
+      whileHover={{ scale: hoverScale }}
       transition={{ duration: 0.1 }}
       {...props}
     >
