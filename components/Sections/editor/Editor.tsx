eb3b4084c7249951924687487c39b2afd2959911
Yuri <yuridev2024@gmail.com>
2024-08-17 12:35:25 +0530

    fix editor FINALLY!

--- components/Sections/editor/Editor.tsx
+++ components/Sections/editor/Editor.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { Compressor } from "@/lib/compressor";
-import Editor, { monaco } from "react-monaco-editor";
+import Editor from "react-monaco-editor";
 import { languageDef, configuration, theme } from '@/lib/editor-config';
 import { useEffect, useState, useRef } from "react";
 
 export default function CCSEditor() {
-  const [code, setCode] = useState(`def module custom-module
+  const defaultCode = `def module custom-module
 def desc "Custom Scripted Module"
 
 on module_enable {
@@ -15,21 +15,18 @@ on module_enable {
 
 on module_disable {
 
-}`);
+}`
+  
   const compressor = new Compressor();
   const [result, setResult] = useState("");
-
-
-  const handleCodeEdit = (value: string, e: any) => {
-    setCode(value);
-  };
+  const [editor, setEditor] = useState<any>();
 
   const deCompressCode = () => {
-    setResult(compressor.decompress(code));
+    setResult(compressor.decompress(editor.getValue()));
   };
 
   const compressCode = () => {
-    setResult(compressor.compress(code));
+    setResult(compressor.compress(editor.getValue()));
   };
 
   const [dark, setDark] = useState(false);
@@ -49,19 +46,16 @@ on module_disable {
 
   const editorWillMount = (monaco: any) => {
     if (!monaco.languages.getLanguages().some(({ id }: any) => id === 'ccs')) {
-      // Register a new language
       monaco.languages.register({ id: 'ccs' });
-      // Register a tokens provider for the language
       monaco.languages.setMonarchTokensProvider('ccs', languageDef);
-      // Set the editing configuration for the language
       monaco.languages.setLanguageConfiguration('ccs', configuration);
-      // Set the theme
       monaco.editor.defineTheme('ccs', theme);
     }
   };
 
   const editorDidMount = (editor: any) => {
     editor.focus();
+    setEditor(editor);
   };
 
   return (
@@ -72,8 +66,7 @@ on module_disable {
           editorWillMount={editorWillMount}
           editorDidMount={editorDidMount}
           className="h-full"
-          value={code}
-          onChange={handleCodeEdit}
+          value={defaultCode}
           theme={dark ? "ccs" : "light"}
           options={{
             'wordWrap': 'on',
@@ -89,8 +82,6 @@ on module_disable {
       <div className="flex-1 h-full">
         <Editor
           language="ccs"
-          editorWillMount={editorWillMount}
-          editorDidMount={editorDidMount}
           className="h-full"
           theme={dark ? "ccs" : "light"}
           value={result}
