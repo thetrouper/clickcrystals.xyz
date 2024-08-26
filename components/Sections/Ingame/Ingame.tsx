e9999b2d94e9dd071dcf63310b000ad890b79fa7
TutlaMC <91965613+TutlaMC@users.noreply.github.com>
2024-08-26 15:54:58 +0530

    Update Ingame.tsx

--- components/Sections/Ingame/Ingame.tsx
+++ components/Sections/Ingame/Ingame.tsx
@@ -13,16 +13,16 @@ export default function Ingame() {
             <Image src={cc} alt="ClickCrystals Client Menu" className="rounded-lg" />
           </div>
           <h1 className="text-center text-gray-700 tracking-tight leading-[1.3] font-extrabold text-2xl md:text-3xl lg:text-4xl my-6">
-            Look the <span className="text-blue-600">ClickCrystals GUI</span>
+            It's time <span className="text-blue-600">you</span>
             <br />
-            but there&apos;s <span className="text-blue-600">MORE</span>, inside!
+            take a &apos;s <span className="text-blue-600">look</span> inside!
           </h1>
           <div className="w-full flex flex-row justify-center">
             <p className="text-gray-500 font-normal m-0 text-center max-w-4xl">
-              Built-in modules, ClickScript, built-in online library to download ClickScripts and directly use them. Give us a try and then ask yourself how you lived without it.
+              We have so many things which we haven't listed out here! Built-in modules, ClickScript IDE, Configs, Renders, Capes & much... much more. Just give us a try and then tell us how you lived without CC?
             </p>
           </div>
-          <div className="text-gray-500 text-sm my-4 italic text-center">P.s. DrDonut loves this mod</div>
+          <div className="text-gray-500 text-sm my-4 italic text-center">Crush CPVP to death with ClickCrystals</div>
           <div className="flex flex-row gap-4 justify-center">
             <GetClickCrystalsButton />
             <MoreScreenshotsButton />
