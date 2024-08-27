e8950f39d4d2173d63b0632a31880d60b023bee6
Ashish <code.with.aasheesh@gmail.com>
2024-08-27 17:59:18 +0530

    add exact count

--- components/Sections/Progress/Progress.tsx
+++ components/Sections/Progress/Progress.tsx
@@ -1,25 +1,26 @@
 import CountUp from "react-countup";
 import { Container } from "@/components/ui/Container";
+import { getTotalDownloads } from "@/lib/getDownloads";
 
-const progress = [
-  {
-    metric: "Total Downloads",
-    value: 100000,
-    postfix: "+",
-  },
-  {
-    metric: "Staff",
-    value: 20,
-    postfix: "+",
-  },
-  {
-    metric: "Years",
-    value: 2,
-    postfix: "+",
-  },
-];
+export default async function Progress() {
+  const progress = [
+    {
+      metric: "Total Downloads",
+      value: await getTotalDownloads(),
+      postfix: "+",
+    },
+    {
+      metric: "Staff",
+      value: 20,
+      postfix: "+",
+    },
+    {
+      metric: "Years",
+      value: 2,
+      postfix: "+",
+    },
+  ];
 
-export default function Progress() {
   return (
     <div className="py-12 bg-gray-800">
       <div className="rounded-md py-8 px-4 lg:px-32 flex gap-4 sm:flex-row flex-col justify-between">
