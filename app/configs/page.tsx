fb0d87b8cd56bedba2d39fc65d19a253165d7426
Ashish <code.with.aasheesh@gmail.com>
2024-08-29 19:08:06 +0530

    show failed to fetch if some fetch fails + WIP configs explorer

---
+++ app/configs/page.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ClickCrystals - Configs Explorer",
+}
+
+import Configs from "@/components/Sections/configs/Configs";
+
+export default function ScriptsArchive() {
+  return (
+    <>
+      <main className="my-12 mx-6 md:mx-24">
+        <div className="py-0">
+          <div className="text-left">
+            <h1 className="text-gray-700 tracking-tight leading-[1.3] font-extrabold text-2xl md:text-3xl lg:text-4xl">
+              ClickCrystals <span className="text-blue-600">Configs Explorer</span>
+            </h1>
+            <p className="text-gray-500 font-normal max-w-4xl my-3">
+              ClickCrystals Configs are used to import/export and share your ClickCrystals module, HUD and core settings with others. Here are some of our configs and you can also share your config by uploading here!
+            </p>
+          </div>
+        </div>
+        <Configs />
+      </main>
+    </>
+  );
+}
