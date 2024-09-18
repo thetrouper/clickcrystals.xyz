307e7b18e1e5930ce47520209711e5237f7c9db9
Ashish <code.with.aasheesh@gmail.com>
2024-09-18 17:15:29 +0530

    edits to navbar and hero
    temporarily remove progress/achievements

--- components/Sections/Hero/Hero.tsx
+++ components/Sections/Hero/Hero.tsx
@@ -9,31 +9,30 @@ import { motion } from 'framer-motion';
 
 export default function Hero() {
   return (
-    <section className="pt-4 lg:pt-0 text-white bg-transparent">
-      <div className="top-0 left-0 absolute w-screen z-[-1] bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#054874,#000000_100%)] h-hero"></div>
-      <div className="py-6 px-8 lg:grid lg:gap-8 lg:grid-cols-12 lg:py-8 lg:h-screen">
-        <div className="place-self-center col-span-5 lg:col-span-7 h-auto py-8 lg:py-0">
-          <h1 className="text-left text-gray-200 tracking-normal leading-[1] font-extrabold text-6xl xl:text-8xl lg:max-w-2xl mb-6 m-0">
+    <section className="py-4 lg:py-2 text-white bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#054874,#000000_100%)]">
+      <div className="py-6 px-8 xl:grid xl:gap-8 xl:grid-cols-12 xl:py-8 xl:min-h-screen">
+        <div className="place-self-center col-span-5 xl:col-span-7 h-auto py-8 xl:py-0">
+          <h1 className="md:text-center xl:text-left text-gray-200 tracking-normal leading-[1] font-extrabold text-6xl md:text-7xl xl:text-8xl xl:max-w-2xl mb-6 m-0">
             Like Steroids,
             <br /> For{' '}
             <span className="bg-[radial-gradient(ellipse_200%_100%_at_top_center,#2f8dbb_100%,#28429f_90%)] text-transparent bg-clip-text">
               Crystal PvP.
             </span>
           </h1>
-          <div className="grid">
-            <p className="text-gray-200/90 font-normal max-w-2xl text-left">
+          <div className="grid md:place-items-center">
+            <p className="text-gray-200/90 font-normal max-w-2xl md:text-center xl:text-left">
               Discover ClickCrystals—a powerful, free, and open-source Minecraft
-              mod designed to enhance your Crystal PvP experience, all without
-              cheats! Loaded with a comprehensive set of modules, you&apos;ll
-              feel like DrDonut 2.0!
+              mod designed to enhance your Crystal PvP gameplay! Loaded with 80+
+              built-in modules and the complete, easiest scripting language for
+              Minecraft ever existed, it's beyond the limits!
             </p>
           </div>
-          <div className="flex flex-row gap-4 mt-4">
+          <div className="flex flex-row items-center md:justify-center xl:justify-start gap-4 my-4">
             <GetClickCrystalsButton />
             <JoinDiscordButton />
           </div>
         </div>
-        <div className="lg:my-4 hidden col-span-5 lg:flex lg:items-center relative">
+        <div className="xl:my-4 hidden col-span-5 xl:flex xl:items-center relative">
           {/* <iframe
             className="aspect-video w-full h-full block"
             src="https://www.youtube.com/embed/-YCPZvNMmFo?si=SzaaGTq-BmCGxRMo"
@@ -44,15 +43,11 @@ export default function Hero() {
           ></iframe> */}
           <motion.img
             src={'/textures/custom/player.png'}
-            style={{
-              imageRendering: 'pixelated',
-            }}
             alt=""
-            className="right-[90px] absolute size-[500px] w-auto"
+            className="right-[90px] absolute h-[500px] w-auto"
           />
         </div>
       </div>
-      <Progress />
     </section>
   );
 }
--- components/ui/Nav/index.tsx
+++ components/ui/Nav/index.tsx
@@ -1,20 +1,35 @@
+'use client';
+
 import { Nav } from '@/components/ui/Nav/Nav';
 import { Title } from '@/components/ui/Nav/Title';
 import { Links } from '@/components/ui/Nav/Links';
 import MobileMenu from './MobileMenu';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    return () => {
+      if (typeof window != null) {
+        window.addEventListener('scroll', () => {
+          setIsTop(window.scrollY === 0 && window.location.pathname === '/');
+        });
+      }
+      setIsTop(window.scrollY === 0 && window.location.pathname === '/');
+    };
+  }, []);
   return (
     <div
-      className="tracking-tight text-white"
+      className="tracking-tight text-white sticky top-0 z-[99999] transition-transform duration-200"
       style={{
-        background: "url('/textures/obsidian.png')",
+        background: isTop ? 'black' : "url('/textures/obsidian.png')",
         backgroundRepeat: 'repeat',
         imageRendering: 'pixelated',
-        backgroundSize: '32px',
+        backgroundSize: '48px',
       }}
     >
-      <header className="w-full duration-100 ease-in flex-none sticky top-0 transition-all z-40">
+      <header className="w-full duration-100 ease-in top-0 transition-all z-40">
         <div className="w-full flex max-w-7xl justify-between md:items-center md:mx-auto md:px-4 md:py-3.5">
           <div className="hidden md:flex justify-between">
             <Title />
