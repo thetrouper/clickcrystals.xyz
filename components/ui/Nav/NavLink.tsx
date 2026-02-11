11b6eb15d157aec8dc88204ecf5ecfaf4e30953c
letmepractice <adinannissar.an@gmail.com>
2026-02-11 00:55:30 +0000

    Clean up NavLink component

--- components/ui/Nav/NavLink.tsx
+++ components/ui/Nav/NavLink.tsx
@@ -10,35 +10,7 @@ type NavLinkProps = {
   onLinkClick?: () => void;
 };
 
-<<<<<<< HEAD
 export const NavLink = ({ label, url, target = '', onLinkClick }: NavLinkProps) => {
-=======
-export const NavLink = ({
-  label,
-  url,
-  target = '',
-  onLinkClick,
-}: NavLinkProps) => {
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const isExternal = target === '_blank' || url.startsWith('http');
-
-  if (isExternal) {
-    return (
-      <Container>
-        <a
-          href={url}
-          target={target}
-          className="transition items-center hover:text-white duration-150 ease-in-out flex font-medium px-4 py-3 transiion-nav"
-          onClick={onLinkClick}
-        >
-          {label}
-        </a>
-      </Container>
-    );
-  }
-
->>>>>>> b81706c21f487605d3176e92abf95aa7509d27a5
   return (
     <Container>
       <Link
