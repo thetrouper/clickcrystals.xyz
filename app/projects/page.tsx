746b1e582c73b9dc28e68a38449ca319a043c0a8
letmepractice <adinannissar.an@gmail.com>
2026-03-07 11:32:09 +0000

    Update styling and fix formatting issues

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,10 +12,10 @@ export default function OtherProjects() {
       <main className="my-12 mx-6 md:mx-24">
         <div className="py-0">
           <div className="text-left">
-            <h1 className="text-gray-700 tracking-tight leading-[1.3] font-extrabold text-2xl md:text-3xl lg:text-4xl">
-              Other <span className="text-blue-600">Projects</span>
+            <h1 className="text-white tracking-tight leading-[1.3] font-bold text-2xl md:text-3xl lg:text-4xl">
+              Other <span className="text-blue-500">Projects</span>
             </h1>
-            <p className="text-gray-500 font-normal max-w-4xl my-3">
+            <p className="text-slate-400 font-normal max-w-4xl my-3">
               Here is a list of some of our, ClickCrystals&apos;s staff projects
               and our official projects.
             </p>
