e89532b52dae10f88dc6d64a89f2574bb7f0d36c
letmepractice <adinannissar.an@gmail.com>
2026-04-29 23:08:36 +0000

    security headers and Next.js patch

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
   compress: true,
   swcMinify: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          { key: 'X-XSS-Protection', value: '1; mode=block' },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'github.com' },
